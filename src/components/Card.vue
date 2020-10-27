d2c455121b4f34a13ca8ab0f11f59cd769c60315
Joe Ciavucco <joe@ciavuc.co>
2020-10-27 12:50:51 +0000

    added some loose styling for card

---
+++ src/components/Card.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="card" :class="card.number ? 'active card__' + provider : ''">
+    <img class="logo" v-if="provider === 1" src="/img/providers/1.svg" />
+    <img class="logo" v-if="provider === 2" src="/img/providers/2.svg" />
+    <img class="logo" v-if="provider === 3" src="/img/providers/3.svg" />
+    <span class="number">{{ card.number }}</span>
+    <span class="name">{{ card.name }}</span>
+    <span class="expiry">{{ card.expiry }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    card: Object,
+    provider: Number,
+  },
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped>
+.card {
+  width: 350px;
+  height: 200px;
+  background: #f7f7f70a;
+  filter: blur(2px);
+  display: block;
+  border-radius: 8px;
+  margin: 30px auto 0 auto;
+  color: #fff;
+  position: relative;
+  transition: 0.5s ease;
+  &.active {
+    filter: blur(0);
+  }
+  .logo {
+    width: auto;
+    height: 40px;
+    position: absolute;
+    top: 10px;
+    right: 10px;
+  }
+  .number {
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
+    left: 15px;
+    right: 0;
+    font-family: "Roboto Mono";
+    letter-spacing: 3px;
+    font-size: 22px;
+    opacity: 0.8;
+  }
+  .name {
+    position: absolute;
+    bottom: 15px;
+    left: 15px;
+  }
+  .expiry {
+    position: absolute;
+    bottom: 15px;
+    right: 15px;
+    font-family: "Roboto Mono";
+    letter-spacing: 1px;
+  }
+
+  &__1 {
+    background-color: blue;
+  }
+
+  &__2 {
+    background-color: #3c3c3c;
+    color: #fff;
+  }
+
+  &__3 {
+    background-color: red;
+  }
+}
+</style>
--- src/components/Payment.vue
+++ src/components/Payment.vue
@@ -1,17 +1,181 @@
 <template>
   <div class="payment">
     <h2>Card Details</h2>
+
+    <Card :card="card" :provider="activeProvider" />
+
+    <form class="form">
+      <div class="form__group">
+        <label for="name">Name on Card</label>
+        <input
+          type="text"
+          name="name"
+          class="card"
+          v-model="card.name"
+          placeholder="Joe Bloggs"
+        />
+      </div>
+
+      <div class="form__group">
+        <label for="number">Card Number</label>
+        <input
+          type="text"
+          name="number"
+          v-model="card.number"
+          v-on:input="handleCardInput()"
+          placeholder="Joe Bloggs"
+        />
+      </div>
+
+      <div class="form__group">
+        <label for="expiry">Card Expiry</label>
+        <input
+          type="text"
+          name="expiry"
+          v-model="card.expiry"
+          placeholder="01/25"
+        />
+      </div>
+
+      <div class="form__group">
+        <label for="security">Card Security Code</label>
+        <input
+          type="text"
+          name="security"
+          v-model="card.security"
+          placeholder="456"
+        />
+      </div>
+    </form>
   </div>
 </template>
 
 <script>
+import $ from "jquery";
+import Card from "./Card";
 export default {
+  components: {
+    Card,
+  },
   data() {
     return {
       product: this.$store.state.activeProduct,
+      card: {
+        name: "",
+        number: "",
+        expiry: "",
+        security: "",
+      },
+      providers: [
+        {
+          brand: "American Express",
+          image: "/images/this.providers/american-express.png",
+          verification: "^3[47][0-9]",
+          separation: "^([0-9]{4})([0-9]{6})?(?:([0-9]{6})([0-9]{5}))?$",
+          hidden: "**** ****** *[0-9][0-9][0-9][0-9]",
+          accepted: true,
+          length: 15,
+        },
+        {
+          brand: "MasterCard",
+          image: "/images/this.providers/mastercard.png",
+          verification: "^5[1-5][0-9]",
+          separation: "^([0-9]{4})([0-9]{4})?([0-9]{4})?([0-9]{4})?$",
+          hidden: "**** **** **** [0-9][0-9][0-9][0-9]",
+          accepted: true,
+          length: 16,
+        },
+        {
+          brand: "Visa",
+          image: "/images/this.providers/visa.png",
+          verification: "^4[0-9]",
+          separation: "^([0-9]{4})([0-9]{4})?([0-9]{4})?([0-9]{4})?$",
+          hidden: "**** **** **** [0-9][0-9][0-9][0-9]",
+          accepted: true,
+          length: 16,
+        },
+        {
+          brand: "Discover",
+          image: "/images/this.providers/discover.png",
+          verification: "^6(?:011|5[0-9]{2})[0-9]",
+          separation: "^([0-9]{4})([0-9]{4})?([0-9]{4})?([0-9]{4})?$",
+          hidden: "**** **** **** [0-9][0-9][0-9][0-9]",
+          accepted: false,
+          length: 16,
+        },
+        {
+          brand: "Diners Club",
+          image: "/images/this.providers/diners-club-international.png",
+          verification: "^3(?:0[0-5]|[68][0-9])[0-9]",
+          separation:
+            "^([0-9]{4})([0-9]{4})?([0-9]{4})?(?:([0-9]{4})([0-9]{4})([0-9]{2}))?$",
+          hidden: "**** **** **[0-9][0-9] [0-9][0-9]",
+          accepted: false,
+          length: 14,
+        },
+        {
+          brand: "JCB",
+          image: "/images/this.providers/jcb.png",
+          verification: "^(?:2131|1800|35[0-9]{3})[0-9]",
+          separation: "^([0-9]{4})([0-9]{4})?([0-9]{4})?([0-9]{4})?$",
+          hidden: "**** **** **** [0-9][0-9][0-9][0-9]",
+          accepted: false,
+          length: 16,
+        },
+      ],
+      activeProvider: null,
+      cardError: null,
     };
   },
-  methods: {},
+  methods: {
+    handleCardInput() {
+      //Preset Data
+      var provider = this.card.number;
+
+      //Find the Credit Card
+      for (var i = 0; i < this.providers.length; i++) {
+        //Check the Type
+        if (provider.match(new RegExp(this.providers[i].verification))) {
+          //Set the Active Card
+          this.activeProvider = i;
+
+          //Add Credit Card Icon
+
+          //If the Credit Card is NOT accepted, Show the Error
+          this.cardError = "Invalid Credit Card (Not accepted)";
+
+          //End the Loop
+          break;
+        }
+      }
+
+      //Show Invalid Card
+      if (
+        this.activeProvider == null &&
+        provider.length > 4 &&
+        $(this).nextAll("small").length == 0
+      ) {
+        //Show Error
+        this.cardError = "Invalid Credit Card";
+      }
+
+      //Preset they Key
+      const key = this.activeProvider !== null ? this.activeProvider : 1;
+
+      //Limit the Length of the Card, Allow Keys
+      if (provider.length >= this.providers[key].length) {
+        return;
+      }
+
+      //Add a Space if the Regex Passes
+      if (new RegExp(this.providers[key].separation).exec(provider)) {
+        this.card.number = this.card.number + " ";
+      }
+
+      //Return
+      return;
+    },
+  },
 };
 </script>
 
@@ -20,7 +184,7 @@ export default {
   position: absolute;
   right: 20px;
   top: 20px;
-  background-color: rgb(22, 26, 32);
+  background-color: #252e3c;
   color: #fff;
   height: 95%;
   width: 450px;
